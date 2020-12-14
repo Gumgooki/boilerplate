98a59de01c11744ccdd23e565c4f90f5bafadec6
Thomas Bunch <56885257+tbunch-datalogics@users.noreply.github.com>
2020-12-14 13:21:57 -0600

    reorganized the index file and added a socket folder

--- server/index.js
+++ server/index.js
@@ -1,51 +1,24 @@
 
 const path = require('path')
-const express = require('express')
+const express = require('express') //used for routing
 const app = express()
 const db = require('./db')
-const session = require('express-session')
+const session = require('express-session') //helps to give us session information thats normally saved in memory
+const morgan = require('morgan') //used for logging
+const bodyParser = require('body-parser') //used for body parsing
+const passport = require('passport') //used for authentication
+const sequelizeStore = require('connect-session-sequelize')(session.Store) //creating a way to connect Sequelize with session data
+const dbStore = new sequelizeStore({db: db}) //configuring and creating our database store so we can save session data directly in DB
+const port = process.env.PORT || 3000;
+const socketio = require('socket.io')
+module.exports = app
 
-
+// keep all of our secret API keys in 'secrets.js'. If we gitignore that file, then nothing will ever show up in git.
 if(process.env.NODE_ENV !== 'production') {
   require('../secrets')
 }
 
-//logging middleware
-const morgan = require('morgan')
-app.use(morgan('dev'))
-
-//serving up static resources from my server
-app.use(express.static(path.join(__dirname, '../public/')))
-
-//need middleware to parse  body for use in req.body
-const bodyParser = require('body-parser')
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
-
-
-//configuring and creating our database store so we can save session data directly in DB
-const sequelizeStore = require('connect-session-sequelize')(session.Store)
-const dbStore = new sequelizeStore({db: db})
-
-//sync the store so our session table gets created
-dbStore.sync()
-
-//this is optional middleware that gives us session information thats normally saved in memory
-app.use(session({
-  secret: process.env.SESSION_SECRET || 'super insecure secret',
-  store: dbStore,
-  resave: false,
-  saveUninitialized: false
-}))
-
-
-//initializing passport to take in req.session and attach user to request obhect
-const passport = require('passport')
-
-app.use(passport.initialize())
-app.use(passport.session())
-
+// passport registration
 passport.serializeUser((user, done) => {
   try {
     done(null, user.id)
@@ -63,32 +36,77 @@ passport.deserializeUser(async (id, done) => {
   }
 })
 
-//this is pulling all the routes from the api folder
-app.use('/api', require('./api'))
-//this is pulling all the routes from the auth folder
-app.use('/auth', require('./auth'))
+const createApp = () => {
+  //logging middleware
+  app.use(morgan('dev'))
 
-//this is sending our index.html file for anything that doesn't match any routes
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../public/index.html')
-)})
+  //need middleware to parse  body for use in req.body
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }))
 
-//so we can log 500 errors
-app.use(function (err, req, res, next){
-  console.log(err)
-  console.log(err.stack)
-  res.status(err.status || 500).send(err.message || 'Internal server error')
-})
 
-//this will start up the server
-const port = process.env.PORT || 3000;
+  //this is optional middleware that gives us session information thats normally saved in memory
+  app.use(session({
+    secret: process.env.SESSION_SECRET || 'super insecure secret',
+    store: dbStore,
+    resave: false,
+    saveUninitialized: false
+  }))
 
-db.sync({force: true})
-.then(
-  app.listen(port, function() {
+  //initializing passport to take in req.session and attach user to request obhect
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+  //this is pulling all the routes from the api folder
+  app.use('/api', require('./api'))
+  //this is pulling all the routes from the auth folder
+  app.use('/auth', require('./auth'))
+
+  //serving up static resources from my server
+  app.use(express.static(path.join(__dirname, '../public/')))
+
+  //this is sending our index.html file for anything that doesn't match any routes
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html')
+  )})
+
+  //so we can log 500 errors
+  app.use(function (err, req, res, next){
+    console.log(err)
+    console.log(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error')
+  })
+}
+
+const startListening = () => {
+  //start listening (also creates a server object representing our server)
+  const server = app.listen(port, () => {
     console.log("Knock, knock")
     console.log("Who's there?")
     console.log(`Your server, listening on port ${port}`)
   })
-)
 
+  const io = socketio(server)
+  require('./socket')(io)
+}
+
+
+
+//sync the database
+const syncDb = () => db.sync({force: true})
+
+
+async function bootApp() {
+  await dbStore.sync()
+  await syncDb()
+  await createApp()
+  await startListening()
+}
+
+
+// if somebody runs file directly from command line, its true; if required by another module, it's false, like if we wanted to use the app in a test spect
+if(require.main === module){
+  bootApp()
+} else {
+  createApp()
+}
